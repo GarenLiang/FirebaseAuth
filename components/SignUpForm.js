df58e2a94feab7586255cccf699f7f40edc206f6
GarenLiang <ideabreeze@gmail.com>
2017-04-19 15:38:51 -0700

    Async and Await :no_mouth:

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -6,14 +6,15 @@ const ROOT_URL = 'https://us-central1-one-time-password-857ce.cloudfunctions.net
 class SignupForm extends Component {
   state = { phone: '' };
 
-  handleSubmit = () => {
-    axios.post(`${ROOT_URL}/createUser`, {
-      phone: this.state.phone
-    })
-      .then(() => {
-        axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone })
-      })
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+    } catch (err) {
+      console.log(err);
+    }
   }
+
   render() {
     return (
       <View>
